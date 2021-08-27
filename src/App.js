8546dc2f7f29505f0fb489b6eef69f30637b4c9b
Nurbolot Boobekov <nurbolot.boobekov.pls@gmail.com>
2021-08-27 18:55:18 +0300

    components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.scss";
 import Content from "./components/content/Content";
-import Header from "./components/header/Header";
+import Header from "./components/Header";
 
 function App() {
   return (
---
+++ src/components/content/Content.jsx
@@ -0,0 +1,414 @@
+import React from "react";
+import Container from "../Container";
+import ContentTop from "./ContentTop";
+
+function Content() {
+  return (
+    <div className="content">
+      <Container>
+        <ContentTop />
+        <h2 className="content__title">Все книги</h2>
+        <div className="content__items">
+          <div className="book-block">
+            <img
+              className="book-block__image"
+              src="https://thumbs.dfs.ivi.ru/storage6/contents/9/2/aa91f2242c0481a4581c3aeb6f4c89.jpg"
+              alt="Book"
+            />
+            <h4 className="book-block__title">Алиса в стране чудес</h4>
+            <div className="book-block__selector">
+              <ul>
+                <li className="active">E-Book</li>
+                <li>Печатная</li>
+              </ul>
+            </div>
+            <div className="book-block__bottom">
+              <div className="book-block__price">600 ₽</div>
+              <div className="button button--outline button--add">
+                <svg
+                  width="12"
+                  height="12"
+                  viewBox="0 0 12 12"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
+                    fill="white"
+                  />
+                </svg>
+                <span>Добавить</span>
+                <i>2</i>
+              </div>
+            </div>
+          </div>
+          <div className="book-block">
+            <img
+              className="book-block__image"
+              src="https://thumbs.dfs.ivi.ru/storage6/contents/9/2/aa91f2242c0481a4581c3aeb6f4c89.jpg"
+              alt="Book"
+            />
+            <h4 className="book-block__title">Алиса в стране чудес</h4>
+            <div className="book-block__selector">
+              <ul>
+                <li className="active">E-Book</li>
+                <li>Печатная</li>
+              </ul>
+            </div>
+            <div className="book-block__bottom">
+              <div className="book-block__price">600 ₽</div>
+              <div className="button button--outline button--add">
+                <svg
+                  width="12"
+                  height="12"
+                  viewBox="0 0 12 12"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
+                    fill="white"
+                  />
+                </svg>
+                <span>Добавить</span>
+                <i>2</i>
+              </div>
+            </div>
+          </div>
+          <div className="book-block">
+            <img
+              className="book-block__image"
+              src="https://thumbs.dfs.ivi.ru/storage6/contents/9/2/aa91f2242c0481a4581c3aeb6f4c89.jpg"
+              alt="Book"
+            />
+            <h4 className="book-block__title">Алиса в стране чудес</h4>
+            <div className="book-block__selector">
+              <ul>
+                <li className="active">E-Book</li>
+                <li>Печатная</li>
+              </ul>
+            </div>
+            <div className="book-block__bottom">
+              <div className="book-block__price">600 ₽</div>
+              <div className="button button--outline button--add">
+                <svg
+                  width="12"
+                  height="12"
+                  viewBox="0 0 12 12"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
+                    fill="white"
+                  />
+                </svg>
+                <span>Добавить</span>
+                <i>2</i>
+              </div>
+            </div>
+          </div>
+          <div className="book-block">
+            <img
+              className="book-block__image"
+              src="https://thumbs.dfs.ivi.ru/storage6/contents/9/2/aa91f2242c0481a4581c3aeb6f4c89.jpg"
+              alt="Book"
+            />
+            <h4 className="book-block__title">Алиса в стране чудес</h4>
+            <div className="book-block__selector">
+              <ul>
+                <li className="active">E-Book</li>
+                <li>Печатная</li>
+              </ul>
+            </div>
+            <div className="book-block__bottom">
+              <div className="book-block__price">600 ₽</div>
+              <div className="button button--outline button--add">
+                <svg
+                  width="12"
+                  height="12"
+                  viewBox="0 0 12 12"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
+                    fill="white"
+                  />
+                </svg>
+                <span>Добавить</span>
+                <i>2</i>
+              </div>
+            </div>
+          </div>
+          <div className="book-block">
+            <img
+              className="book-block__image"
+              src="https://thumbs.dfs.ivi.ru/storage6/contents/9/2/aa91f2242c0481a4581c3aeb6f4c89.jpg"
+              alt="Book"
+            />
+            <h4 className="book-block__title">Алиса в стране чудес</h4>
+            <div className="book-block__selector">
+              <ul>
+                <li className="active">E-Book</li>
+                <li>Печатная</li>
+              </ul>
+            </div>
+            <div className="book-block__bottom">
+              <div className="book-block__price">600 ₽</div>
+              <div className="button button--outline button--add">
+                <svg
+                  width="12"
+                  height="12"
+                  viewBox="0 0 12 12"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
+                    fill="white"
+                  />
+                </svg>
+                <span>Добавить</span>
+                <i>2</i>
+              </div>
+            </div>
+          </div>
+          <div className="book-block">
+            <img
+              className="book-block__image"
+              src="https://thumbs.dfs.ivi.ru/storage6/contents/9/2/aa91f2242c0481a4581c3aeb6f4c89.jpg"
+              alt="Book"
+            />
+            <h4 className="book-block__title">Алиса в стране чудес</h4>
+            <div className="book-block__selector">
+              <ul>
+                <li className="active">E-Book</li>
+                <li>Печатная</li>
+              </ul>
+            </div>
+            <div className="book-block__bottom">
+              <div className="book-block__price">600 ₽</div>
+              <div className="button button--outline button--add">
+                <svg
+                  width="12"
+                  height="12"
+                  viewBox="0 0 12 12"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
+                    fill="white"
+                  />
+                </svg>
+                <span>Добавить</span>
+                <i>2</i>
+              </div>
+            </div>
+          </div>
+          <div className="book-block">
+            <img
+              className="book-block__image"
+              src="https://thumbs.dfs.ivi.ru/storage6/contents/9/2/aa91f2242c0481a4581c3aeb6f4c89.jpg"
+              alt="Book"
+            />
+            <h4 className="book-block__title">Алиса в стране чудес</h4>
+            <div className="book-block__selector">
+              <ul>
+                <li className="active">E-Book</li>
+                <li>Печатная</li>
+              </ul>
+            </div>
+            <div className="book-block__bottom">
+              <div className="book-block__price">600 ₽</div>
+              <div className="button button--outline button--add">
+                <svg
+                  width="12"
+                  height="12"
+                  viewBox="0 0 12 12"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
+                    fill="white"
+                  />
+                </svg>
+                <span>Добавить</span>
+                <i>2</i>
+              </div>
+            </div>
+          </div>
+          <div className="book-block">
+            <img
+              className="book-block__image"
+              src="https://thumbs.dfs.ivi.ru/storage6/contents/9/2/aa91f2242c0481a4581c3aeb6f4c89.jpg"
+              alt="Book"
+            />
+            <h4 className="book-block__title">Алиса в стране чудес</h4>
+            <div className="book-block__selector">
+              <ul>
+                <li className="active">E-Book</li>
+                <li>Печатная</li>
+              </ul>
+            </div>
+            <div className="book-block__bottom">
+              <div className="book-block__price">600 ₽</div>
+              <div className="button button--outline button--add">
+                <svg
+                  width="12"
+                  height="12"
+                  viewBox="0 0 12 12"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
+                    fill="white"
+                  />
+                </svg>
+                <span>Добавить</span>
+                <i>2</i>
+              </div>
+            </div>
+          </div>
+          <div className="book-block">
+            <img
+              className="book-block__image"
+              src="https://thumbs.dfs.ivi.ru/storage6/contents/9/2/aa91f2242c0481a4581c3aeb6f4c89.jpg"
+              alt="Book"
+            />
+            <h4 className="book-block__title">Алиса в стране чудес</h4>
+            <div className="book-block__selector">
+              <ul>
+                <li className="active">E-Book</li>
+                <li>Печатная</li>
+              </ul>
+            </div>
+            <div className="book-block__bottom">
+              <div className="book-block__price">600 ₽</div>
+              <div className="button button--outline button--add">
+                <svg
+                  width="12"
+                  height="12"
+                  viewBox="0 0 12 12"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
+                    fill="white"
+                  />
+                </svg>
+                <span>Добавить</span>
+                <i>2</i>
+              </div>
+            </div>
+          </div>
+          <div className="book-block">
+            <img
+              className="book-block__image"
+              src="https://thumbs.dfs.ivi.ru/storage6/contents/9/2/aa91f2242c0481a4581c3aeb6f4c89.jpg"
+              alt="Book"
+            />
+            <h4 className="book-block__title">Алиса в стране чудес</h4>
+            <div className="book-block__selector">
+              <ul>
+                <li className="active">E-Book</li>
+                <li>Печатная</li>
+              </ul>
+            </div>
+            <div className="book-block__bottom">
+              <div className="book-block__price">600 ₽</div>
+              <div className="button button--outline button--add">
+                <svg
+                  width="12"
+                  height="12"
+                  viewBox="0 0 12 12"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
+                    fill="white"
+                  />
+                </svg>
+                <span>Добавить</span>
+                <i>2</i>
+              </div>
+            </div>
+          </div>
+          <div className="book-block">
+            <img
+              className="book-block__image"
+              src="https://thumbs.dfs.ivi.ru/storage6/contents/9/2/aa91f2242c0481a4581c3aeb6f4c89.jpg"
+              alt="Book"
+            />
+            <h4 className="book-block__title">Алиса в стране чудес</h4>
+            <div className="book-block__selector">
+              <ul>
+                <li className="active">E-Book</li>
+                <li>Печатная</li>
+              </ul>
+            </div>
+            <div className="book-block__bottom">
+              <div className="book-block__price">600 ₽</div>
+              <div className="button button--outline button--add">
+                <svg
+                  width="12"
+                  height="12"
+                  viewBox="0 0 12 12"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
+                    fill="white"
+                  />
+                </svg>
+                <span>Добавить</span>
+                <i>2</i>
+              </div>
+            </div>
+          </div>
+          <div className="book-block">
+            <img
+              className="book-block__image"
+              src="https://thumbs.dfs.ivi.ru/storage6/contents/9/2/aa91f2242c0481a4581c3aeb6f4c89.jpg"
+              alt="Book"
+            />
+            <h4 className="book-block__title">Алиса в стране чудес</h4>
+            <div className="book-block__selector">
+              <ul>
+                <li className="active">E-Book</li>
+                <li>Печатная</li>
+              </ul>
+            </div>
+            <div className="book-block__bottom">
+              <div className="book-block__price">600 ₽</div>
+              <div className="button button--outline button--add">
+                <svg
+                  width="12"
+                  height="12"
+                  viewBox="0 0 12 12"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
+                    fill="white"
+                  />
+                </svg>
+                <span>Добавить</span>
+                <i>2</i>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+}
+
+export default Content;
