4f59822a9448738b043f8644b206631c7a48d5cd
Nurbolot Boobekov <nurbolot.boobekov.pls@gmail.com>
2021-08-31 12:27:03 +0300

    Add Sort.jsx and index.js

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Container } from "./Container";
+export { default as Header } from "./Header";
+export { default as Logo } from "./Logo";
+export { default as Button } from "./Button";
+export { default as Categories } from "./Categories";
+export { default as Sort } from "./Sort";
